fa600d597641e754c563020a2f569d1bb5406528
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-27 09:09:24 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,2 +1,4 @@
 //Importar a dependencia do Sqlite3
-const sqlite3 = require["sqlite3"]
+const sqlite3 = require["sqlite3"].verbose()
+//Cria o objeto que vai fazer operações nos bancos de dados
+ const db = new squile3.Database("./src/database/database.db")
