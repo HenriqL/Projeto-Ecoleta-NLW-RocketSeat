ec4b93c4966da6669682fdefe46d37b469779b61
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-28 10:55:23 -0300

    Finalização do db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,55 +3,72 @@ const sqlite3 = require("sqlite3").verbose()
 //Cria o objeto que vai fazer operações nos bancos de dados
  const db = new sqlite3.Database("./src/database/database.db")
 
+module.exports = db
+
 //Ultilza o objto de Banco de dados para as operações
-db.serialize(() => {
-    //cria a tabela com comandos SQL
-    db.run(`
-            CREATE TABLE IF NOT EXISTS places (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                image TEXT,
-                name TEXT,
-                address TEXT,
-                address2 TEXT,
-                state TEXT,
-                city TEXT,
-                items TEXT
-            );
-    `)
-    //inserir dados 
-    // O VALUE fica como interroção, pois não sabemos o valor que vai ser inserido
-    const query = `
-            INSERT INTO places (
-                image,
-                name,
-                address,
-                address2,
-                state,
-                city,
-                items
-            ) VALUES (?, ?, ?, ?, ?, ?, ?); 
-        `
+// db.serialize(() => {
+//     //cria a tabela com comandos SQL
+//     db.run(`
+//             CREATE TABLE IF NOT EXISTS places (
+//                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+//                 image TEXT,
+//                 name TEXT,
+//                 address TEXT,
+//                 address2 TEXT,
+//                 state TEXT,
+//                 city TEXT,
+//                 items TEXT
+//             );
+//     `)
+//     //inserir dados 
+//     // O VALUE fica como interroção, pois não sabemos o valor que vai ser inserido
+//     const query = `
+//             INSERT INTO places (
+//                 image,
+//                 name,
+//                 address,
+//                 address2,
+//                 state,
+//                 city,
+//                 items
+//             ) VALUES (?, ?, ?, ?, ?, ?, ?); 
+//         `
 
-    const values = [
-        "https://images.unsplash.com/photo-1574689049377-2d1c02921490?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-        "Colectoria",
-        "Guilherme Gemballa, Jardin América",
-        "Nº 260",
-        "Santa Catarina",
-        "Rio do Sul",
-        "Resíduos Eletrônicos, Lâmpadas"
-    ]
+//     const values = [
+//         "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
+//         "Papersider",
+//         "Guilherme Gemballa, Jardin América",
+//         "Nº 260",
+//         "Santa Catarina",
+//         "Rio do Sul",
+//         "Papeis e Papelão"
+//     ]
 
-    function afterInsertData(err){
-        if(err){
-            return console.log(err)
-        }
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
+//     function afterInsertData(err){
+//         if(err){
+//             return console.log(err)
+//         }
+//         console.log("Cadastrado com sucesso")
+//         console.log(this)
+//     }
 
-    db.run(query, values, afterInsertData)
-    //consulta os dados
+//     db.run(query, values, afterInsertData)
 
-    //deleta os dados da tabela
-})
+//     //consulta os dados
+
+//     // db.all(`SELECT * FROM places`, function(err, rows){
+//     //     if(err){
+//     //         return console.log(err)
+//     //     }console.log("Aqui estão seus registros")
+//     //     console.log(rows)
+//     // })
+
+//     // //deleta os dados da tabela
+//     // db.run(`DELETE FROM places WHERE id = ?`, [3], function(err){
+//     //     if(err){
+//     //         return console.log(err)
+//     //     }console.log("Registro deletado com sucesso")
+//     // })
+
+    
+// })
