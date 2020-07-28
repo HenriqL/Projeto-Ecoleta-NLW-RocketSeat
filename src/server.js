ebca098f1bc706ead3ffa444b59f689417b487e5
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-28 11:05:32 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,6 +26,16 @@ server.get("/create-point", (req, res) =>{
 })
 //Search-Result
 server.get("/search", (req, res) =>{
+
+  //consulta os dados
+
+db.all(`SELECT * FROM places`, function(err, rows){
+        if(err){
+            return console.log(err)
+        }console.log("Aqui estão seus registros")
+        console.log(rows)
+    })
+
   res.render("search-results.html")
 })
 
