6615775130a1463ba966b178c2e66c043a57a83d
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-28 12:37:17 -0300

    correção de erros

--- src/database/db.js
+++ src/database/db.js
@@ -64,7 +64,7 @@ module.exports = db
 //     // })
 
 //     // //deleta os dados da tabela
-    // db.run(`DELETE FROM places WHERE id = ?`, [5], function(err){
+    // db.run(`DELETE FROM places WHERE id = ?`, [9], function(err){
     //     if(err){
     //         return console.log(err)
     //     }console.log("Registro deletado com sucesso")
--- src/server.js
+++ src/server.js
@@ -33,9 +33,45 @@ server.get("/create-point", (req, res) =>{
 })
 
 server.post("/savepoint", (req, res) =>{
-  console.log(req.body)
+  // console.log(req.body)
+  //inserir os dados na tabela 
 
-  return res.send("ok")
+  const query = `
+               INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+             ) VALUES (?, ?, ?, ?, ?, ?, ?); 
+              `
+  
+      const values = [
+          req.body.image,
+          req.body.name,
+          req.body.address,
+          req.body.adress2,
+          req.body.state,
+          req.body.city,
+          req.body.items
+      ]
+  
+      function afterInsertData(err){
+          if(err){
+              return console.log(err)
+          }
+          console.log("Cadastrado com sucesso")
+          console.log(this)
+          return res.send("ok")
+      }
+  
+      db.run(query, values, afterInsertData)
+
+
+
+  
 })
 
 //Search-Result
